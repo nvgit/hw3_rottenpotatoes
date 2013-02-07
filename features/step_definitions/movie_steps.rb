9618639838bf3c427deb33a2cbf3680be88a23b2
nvgit <nizar@post.harvard.edu>
2013-02-07 05:54:29 -0800

    hw3-part1a

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,12 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    #Movie.find_by_title_and_rating(movie[:title], movie[:rating]).inspect
+    if !Movie.find_by_title_and_rating(movie[:title], movie[:rating]) then
+      Movie.create!(movie)
+    end
   end
-  flunk "Unimplemented"
+  assert (Movie.count >= movies_table.hashes.count), "Error while DB filling"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -25,4 +29,12 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split.each do |rating|
+    if uncheck then
+      step %{I uncheck "ratings[#{rating}]"}
+    else
+      step %{I check "ratings[#{rating}]"}
+    end
+  end
 end
+
